39243f8549dd9aa9885c641b23f8d6ba8562e625
Temirkul <temirkule@gmail.com>
2024-05-26 06:36:59 +0500

    [fix]: can connect to localhost now (server launches)

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,4 +1,5 @@
 import random
+from typing import Any
 from PIL import Image
 from fastapi import APIRouter, Depends
 from .schemas import StoryResponse, StoryInput
@@ -37,8 +38,8 @@ async def continue_story(story_input: StoryInput) -> StoryResponse:
     return StoryResponse(story_text=response)
 
 
-@router.get("/generate-image-pipeline")
-async def generate_image_pipeline(story_at_current_timestep: str) -> Image.Image:
+@router.get("/generate-image-pipeline", response_model=Any)  # pydantic/fastapi don't work well with PIL Image for some reason
+async def generate_image_pipeline(story_at_current_timestep: str) -> Image.Image | None:
     generate_image_chain = get_generate_image_chain()
     image_prompt = await generate_image_chain.ainvoke(story_at_current_timestep)
     image = generate_image(image_prompt)
